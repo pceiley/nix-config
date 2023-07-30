b2ad51e385c90050118e5fce60925a46938baa5a
Peter Ceiley <pgceiley@gmail.com>
2023-07-30 14:47:39 +1000

    move some config into configuration.nix from hardware-configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -84,6 +84,18 @@
     keyMap = "us";
   };
 
+  zramSwap.enable = true;
+
+  # NIC configuration
+  networking.useDHCP = lib.mkDefault false;
+  # networking.interfaces.eno1.useDHCP = lib.mkDefault true;
+  networking.interfaces.eno1.ipv4.addresses = [ {
+    address = "192.168.10.5";
+    prefixLength = 24;
+  } ];
+  networking.defaultGateway = "192.168.10.254";
+  networking.nameservers = [ "192.168.10.254" ];
+
   # Custom filesystem config
   systemd.tmpfiles.rules = [
     "d /secrets 0750 root wheel"
