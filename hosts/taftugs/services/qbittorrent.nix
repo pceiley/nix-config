ff1ef18882141836dd01995db7037aa1815916e3
Peter Ceiley <pgceiley@gmail.com>
2023-08-28 21:29:13 +1000

    add script to fix permissions after qbittorrent download

--- hosts/taftugs/services/qbittorrent.nix
+++ hosts/taftugs/services/qbittorrent.nix
@@ -1,5 +1,18 @@
+# qBittorrent service activation
+#
+# The shell script 'fixdlperms' is also created and should be added to the
+# "Run external program on finished" section with the full path:
+# /run/current-system/sw/bin/fixdlperms
+
 { pkgs, ... }:
 
+let
+  downloadDir = "/data/multimedia/downloads";
+  fixDownloadPerms = pkgs.writeShellScriptBin "fixdlperms" ''
+    find ${downloadDir} -type d -exec chmod 2775 {} +
+    find ${downloadDir} -type f -exec chmod 0664 {} +
+  '';
+in
 {
   services.qbittorrent = {
     enable = true;
@@ -10,4 +23,6 @@
 
   # Allow qbittorrent to save files in the multimedia share
   users.users.qbittorrent.extraGroups = [ "multimedia" ];
+
+  environment.systemPackages = [ fixDownloadPerms ];
 }
